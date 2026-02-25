0b828bd2b58345ef8ee61ebd3640e6e10934630d
TapanRoyal31 <tapanr3105@gmail.com>
2026-02-25 10:26:51 +0530

    "Room Pages in Front"

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import AllRoom from "./pages/AllRoom";
+import RoomDetails from "./pages/RoomDetails";
+
+const App = () => {
+  const isOwnerPath = useLocation().pathname.includes("owner");
+
+  return (
+    <div>
+      {!isOwnerPath && <Navbar />}
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<AllRoom />} />
+          <Route path="/rooms/:id" element={<RoomDetails />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/pages/RoomDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RoomDetails = () => {
+  return (
+    <div>
+      <h1 className=''>Room Details</h1>
+    </div>
+  )
+}
+
+export default RoomDetails
