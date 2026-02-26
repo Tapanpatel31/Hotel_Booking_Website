d61f1ce8b93cd57b7e5cf03e928dfa46797aa8d8
TapanRoyal31 <tapanr3105@gmail.com>
2026-02-26 12:18:30 +0530

    "room detail page completed"

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -115,9 +115,9 @@ export const roomCommonData = [
 // User Dummy Data
 export const userDummyData = {
     "_id": "user_2unqyL4diJFP1E3pIBnasc7w8hP",
-    "username": "Great Stack",
-    "email": "user.greatstack@gmail.com",
-    "image": "https://img.clerk.com/eyJ0eXBlIjoicHJveHkiLCJzcmMiOiJodHRwczovL2ltYWdlcy5jbGVyay5kZXYvdXBsb2FkZWQvaW1nXzJ2N2c5YVpSSEFVYVUxbmVYZ2JkSVVuWnFzWSJ9",
+    "username": "Quick Stay",
+    "email": "user.quickstay@gmail.com",
+    "image": "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
     "role": "hotelOwner",
     "createdAt": "2025-03-25T09:29:16.367Z",
     "updatedAt": "2025-04-10T06:34:48.719Z",
